19d2570c36fc6e4ed20eebb06cbaafaead9e115c
hivespaula <paula.hives@weareams.comm>
2022-11-10 09:21:13 +0000

    Create db models

---
+++ application/models.py
@@ -0,0 +1,21 @@
+#database models
+from application import db
+
+#one character can have many objectives
+
+class Characters(db.Model):
+    __tablename__ = 'characters'
+    id = db.Column(db.Integer, primary_key=True)
+    char_name = db.Column(db.String(30), nullable=False)
+    char_lvl = db.Column(db.Integer, nullable=False)
+    char_proffesion = db.Column(db.String(30), nullable=False)
+    objectives = db.relationship('Objectives', backref='objectivesbr')
+
+
+class Objectives(db.Model):
+    __tablename__ = 'objectives'
+    id = db.Column(db.Integer, primary_key=True)
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'), nullable=False)
+    daily = db.Column(db.Boolean, nullable=False)
+    weekly = db.Column(db.Boolean, nullable=False)
+    proffesion = db.Column(db.Boolean, nullable=False)
