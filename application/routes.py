55bd2149973b9e33938922f8f426b61a713588b4
hivespaula <paula.hives@weareams.comm>
2022-11-11 10:10:39 +0000

    Update routes

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,15 @@
 from application import app, db
 from application.models import Characters, Objectives
 from flask import render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from application.forms import AddCharacter
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    form = AddCharacter()
+    return render_template('index.html', form=form)
+
+# @app.route('/objectives', methods = ['GET'])
+# def objectives():
+#     objectives = Objectives.query.all()
+#     return render_template('index.html', form=form)
