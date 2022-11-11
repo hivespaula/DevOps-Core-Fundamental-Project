5d808c8e31deadd779363225eca93e808d2b7781
hivespaula <paula.hives@weareams.comm>
2022-11-11 10:04:10 +0000

    Add SECRET_KEY

--- application/__init__.py
+++ application/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import os
+from os import getenv
 
 app = Flask(__name__)
 #'mysql+pymysql://user:password@123.45.6.78:3306/mydatabase'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
