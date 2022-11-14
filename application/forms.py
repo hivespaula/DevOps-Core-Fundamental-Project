2c25bc76d5803ba628e885325577ca6b7530d9d5
hivespaula <paula.hives@weareams.comm>
2022-11-14 15:58:31 +0000

    Add UpdateCharacter form

--- application/forms.py
+++ application/forms.py
@@ -11,6 +11,16 @@ class AddCharacter(FlaskForm):
         ('Leatherworking', 'Leatherworking'), ('Tailoring', 'Tailoring')])
     submit = SubmitField('Add Character')
 
+class UpdateCharacter(FlaskForm):
+    name = StringField('Character Name')
+    lvl = IntegerField('Character level')
+    proffesion = SelectField('Proffesion', choices=[
+        ('Alchemy','Alchemy'), ('Engineering','Engineering'), 
+        ('Enchanting', 'Enchanting'), ('Blacksmithing', 'Blacksmithing'),
+        ('Inscription','Inscription'), ('Jewelcrafting', 'Jewelcrafting'), 
+        ('Leatherworking', 'Leatherworking'), ('Tailoring', 'Tailoring')])
+    submit = SubmitField('Update Character')
+
 class Objectives(FlaskForm):
     daily_quest = BooleanField('Daily quest', default=False)
     weekly_quest = BooleanField('Weekly quest', default=False)
